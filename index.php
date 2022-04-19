9a0eedcf3169326bec3922d7258c47bb961346dc
Aref Rezaei <arra4040@gmail.com>
2022-04-19 10:37:07 +0430

    implement header

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>وبلاگ من</title>
+</head>
+<body>
+    <div class="header"><!-- start header -->
+        <div class="container"><!-- start container -->
+            <ul class="menu">
+                <li><a href="#">صفحه اصلی</a></li>
+                <li><a href="#">برنامه نویسی</a></li>
+                <li><a href="#">گرافیک</a></li>
+                <li><a href="#">ورود مدیریت</a></li>
+            </ul>
+            <div class="clear"></div>
+        </div><!-- end container -->
+
+    </div><!-- end header -->
+    <div class="clear"></div>
+    <div class="body"><!-- start footer -->
+    
+    </div><!-- end body -->
+
+    <div class="footer"><!-- start footer -->
+    
+    </div><!-- end footer-->
+</body>
+</html>
