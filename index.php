c970be1cd0ac8610805b75e56f358cfb978245c9
Aref Rezaei <arra4040@gmail.com>
2022-04-27 10:56:03 +0430

    first page post boxes added

--- index.php
+++ index.php
@@ -35,7 +35,144 @@
     <div class="clear"></div>
     <div class="body"><!-- start body -->
         <div class="container">
+            
+            <div class="post"><!-- start post -->
+                <div class="postHeader"><!-- start postHeader -->
+                    <h1 class="postTitle">اولین مطلب</h1>
+                    <span>تاریخ انتشار: 1401/02/05</span>
+                    <div class="clear"></div>
+                </div><!-- end postHeader -->
+                <div class="postBody"><!-- start postBody -->
+                    <div class="postImage"><!-- start postImage -->
+                        <img src="https://picsum.photos/300/300" alt="">
+                    </div><!-- end postImage -->
+                    <div class="postDesc"><!-- start postDesc -->
+                        <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p>
+                    </div><!-- end postDesc -->
+                    <div class="clear"></div>
+                </div><!-- end postBody -->
+                <div class="postFooter"><!-- start postFooter -->
+                    <span>نویسنده مطلب: لورم ایپسوم</span>
+                    <a href="" class="readMore">ادامه مطلب</a>
+                    <div class="clear"></div>
+                </div><!-- end postFooter -->
+                <div class="clear"></div>
+            </div><!-- end post -->
 
+            <div class="post"><!-- start post -->
+                <div class="postHeader"><!-- start postHeader -->
+                    <h1 class="postTitle">اولین مطلب</h1>
+                    <span>تاریخ انتشار: 1401/02/05</span>
+                    <div class="clear"></div>
+                </div><!-- end postHeader -->
+                <div class="postBody"><!-- start postBody -->
+                    <div class="postImage"><!-- start postImage -->
+                        <img src="https://picsum.photos/300/300" alt="">
+                    </div><!-- end postImage -->
+                    <div class="postDesc"><!-- start postDesc -->
+                        <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p>
+                    </div><!-- end postDesc -->
+                    <div class="clear"></div>
+                </div><!-- end postBody -->
+                <div class="postFooter"><!-- start postFooter -->
+                    <span>نویسنده مطلب: لورم ایپسوم</span>
+                    <a href="" class="readMore">ادامه مطلب</a>
+                    <div class="clear"></div>
+                </div><!-- end postFooter -->
+                <div class="clear"></div>
+            </div><!-- end post -->
+
+            <div class="post"><!-- start post -->
+                <div class="postHeader"><!-- start postHeader -->
+                    <h1 class="postTitle">اولین مطلب</h1>
+                    <span>تاریخ انتشار: 1401/02/05</span>
+                    <div class="clear"></div>
+                </div><!-- end postHeader -->
+                <div class="postBody"><!-- start postBody -->
+                    <div class="postImage"><!-- start postImage -->
+                        <img src="https://picsum.photos/300/300" alt="">
+                    </div><!-- end postImage -->
+                    <div class="postDesc"><!-- start postDesc -->
+                        <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p>
+                    </div><!-- end postDesc -->
+                    <div class="clear"></div>
+                </div><!-- end postBody -->
+                <div class="postFooter"><!-- start postFooter -->
+                    <span>نویسنده مطلب: لورم ایپسوم</span>
+                    <a href="" class="readMore">ادامه مطلب</a>
+                    <div class="clear"></div>
+                </div><!-- end postFooter -->
+                <div class="clear"></div>
+            </div><!-- end post -->
+
+            <div class="post"><!-- start post -->
+                <div class="postHeader"><!-- start postHeader -->
+                    <h1 class="postTitle">اولین مطلب</h1>
+                    <span>تاریخ انتشار: 1401/02/05</span>
+                    <div class="clear"></div>
+                </div><!-- end postHeader -->
+                <div class="postBody"><!-- start postBody -->
+                    <div class="postImage"><!-- start postImage -->
+                        <img src="https://picsum.photos/300/300" alt="">
+                    </div><!-- end postImage -->
+                    <div class="postDesc"><!-- start postDesc -->
+                        <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p>
+                    </div><!-- end postDesc -->
+                    <div class="clear"></div>
+                </div><!-- end postBody -->
+                <div class="postFooter"><!-- start postFooter -->
+                    <span>نویسنده مطلب: لورم ایپسوم</span>
+                    <a href="" class="readMore">ادامه مطلب</a>
+                    <div class="clear"></div>
+                </div><!-- end postFooter -->
+                <div class="clear"></div>
+            </div><!-- end post -->
+
+            <div class="post"><!-- start post -->
+                <div class="postHeader"><!-- start postHeader -->
+                    <h1 class="postTitle">اولین مطلب</h1>
+                    <span>تاریخ انتشار: 1401/02/05</span>
+                    <div class="clear"></div>
+                </div><!-- end postHeader -->
+                <div class="postBody"><!-- start postBody -->
+                    <div class="postImage"><!-- start postImage -->
+                        <img src="https://picsum.photos/300/300" alt="">
+                    </div><!-- end postImage -->
+                    <div class="postDesc"><!-- start postDesc -->
+                        <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p>
+                    </div><!-- end postDesc -->
+                    <div class="clear"></div>
+                </div><!-- end postBody -->
+                <div class="postFooter"><!-- start postFooter -->
+                    <span>نویسنده مطلب: لورم ایپسوم</span>
+                    <a href="" class="readMore">ادامه مطلب</a>
+                    <div class="clear"></div>
+                </div><!-- end postFooter -->
+                <div class="clear"></div>
+            </div><!-- end post -->
+
+            <div class="post"><!-- start post -->
+                <div class="postHeader"><!-- start postHeader -->
+                    <h1 class="postTitle">اولین مطلب</h1>
+                    <span>تاریخ انتشار: 1401/02/05</span>
+                    <div class="clear"></div>
+                </div><!-- end postHeader -->
+                <div class="postBody"><!-- start postBody -->
+                    <div class="postImage"><!-- start postImage -->
+                        <img src="https://picsum.photos/300/300" alt="">
+                    </div><!-- end postImage -->
+                    <div class="postDesc"><!-- start postDesc -->
+                        <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p>
+                    </div><!-- end postDesc -->
+                    <div class="clear"></div>
+                </div><!-- end postBody -->
+                <div class="postFooter"><!-- start postFooter -->
+                    <span>نویسنده مطلب: لورم ایپسوم</span>
+                    <a href="" class="readMore">ادامه مطلب</a>
+                    <div class="clear"></div>
+                </div><!-- end postFooter -->
+                <div class="clear"></div>
+            </div><!-- end post -->
             <div class="clear"></div>
         </div>
         <div class="clear"></div>
